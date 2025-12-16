53596aa164fbb6ca42cd38a39d6cf3af60f2d8f2
AREAFISIO <direzione@areafisiobologna.it>
2025-12-16 14:31:50 +0100

    Update auth-me.js

--- api/auth-me.js
+++ api/auth-me.js
@@ -1,9 +1,6 @@
-import { requireSession } from "./_auth.js";
+import { requireSession, setJson } from "./_auth.js";
 
 export default async function handler(req, res) {
   const session = requireSession(req);
-
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({ ok: Boolean(session), session: session || null }));
+  return setJson(res, 200, { ok: Boolean(session), session: session || null });
 }
